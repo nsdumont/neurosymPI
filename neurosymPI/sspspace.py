e19ffcbeb999f852d7c57af283e1cdad07300534
Nicole Dumont <nicole.s.dumont@gmail.com>
2022-10-17 13:16:34 -0400

    Added more comments

--- neurosymPI/sspspace.py
+++ neurosymPI/sspspace.py
@@ -4,13 +4,35 @@ from scipy.stats import qmc
 from scipy.stats import special_ortho_group
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
-
 import warnings
 
+
+####################################
+#  SSP space classes used in paper #
+####################################
+
+# An SSP S(x) is a d-dim real-valued vector that represents a n-dim varaible x
+# where n<<d, i.e. we are projecting x into a high dimensional vector space
+# Search "hyperdimensional computing" or "vector symbolic architectures" to learn more about this approach in general
+# Or read " Simulating and Predicting Dynamical Systems With Spatial Semantic Pointers" to learn details about our particular approach
+
+#########################
+#  SSPSpace base class  #
+#########################
+# An SSP vector space is \mathcal{S} = {S(x)=IDFT(exp(iAx/l)) \in R^d | x \in \mathcal{X}} where \mathcal{X} is a subset of R^n, and 
+#   A \in R^{d x n} is a fixed phase matrix, and l \in \mathcal{R}^n are the length scale parameters
+# exp(iAx) will be a vector of phasors. The phases of the phasors is given by the similarity/dot product between x and the row vectors that make up A
 class SSPSpace:
     def __init__(self, domain_dim: int, ssp_dim: int, axis_matrix=None, phase_matrix=None,
                  domain_bounds=None, length_scale=1):
-        
+        # A general way to specfiy a SSP vector space requries
+        #   domain_dim: n, the dimensionality of the varaibles represented by the SSP (x)
+        #   ssp_dim: d, the dimensionality of the SSP representation itself
+        #   phase_matrix (optional if axis_matrix given): the SSP encoding is defined by the dxn matrix A. This can be provided as input via this arg
+        #   axis_matrix (optional if phase_matrix given): this can be provided instead of phase_matrix. This is IDFT(exp(iA)) \in R^{d x n}. Sometimes it is preferable to define the SSP with this
+        #   domain_bounds (optional): the bounds of the \mathcal{X}. 
+        #           (SSPs can represent x from an unbounded space but decoding x from S(x) requries either bounds or a good initial guess. Also, the plotting methods requrie bounds to be given)
+        #   length_scale (default=1): l, the length scale parameters. A decent rule of thumb is that if your bounds are [-b,b] then use l=b/10
         
         self.sample_points = None
         self.sample_ssps = None
@@ -37,7 +59,8 @@ class SSPSpace:
             assert phase_matrix.shape[1] == domain_dim
             self.phase_matrix = phase_matrix
             self.axis_matrix = np.fft.ifft(np.exp(1.j*phase_matrix), axis=0).real
-            
+        
+    # Updating the length scale. In some application you might want the length scale to be learned online
     def update_lengthscale(self, scale):
         if not isinstance(scale, np.ndarray) or scale.size == 1:
             self.length_scale = scale * np.ones((self.domain_dim,))
@@ -46,6 +69,7 @@ class SSPSpace:
             self.length_scale = scale
         assert self.length_scale.size == self.domain_dim
     
+    # Encoding a varaible x. Given x (\in R^{n x m} where m is the number of points to be encoded) as input, return SSPs S(x) \in R^{d x m}
     def encode(self,x):
         assert x.shape[0] == self.domain_dim
         ls_mat = np.atleast_2d(np.diag(1/self.length_scale.flatten()))
@@ -53,6 +77,7 @@ class SSPSpace:
         data = np.fft.ifft( np.exp( 1.j * self.phase_matrix @ scaled_x ), axis=0 ).real
         return data
     
+    # Encode x in the Fourier domain. Will return exp(iAx/l) (i.e. DFT{S(x)})
     def encode_fourier(self,x):
         assert x.shape[0] == self.domain_dim
         ls_mat = np.atleast_2d(np.diag(1/self.length_scale.flatten()))
@@ -60,7 +85,10 @@ class SSPSpace:
         data =  np.exp( 1.j * self.phase_matrix @ scaled_x )
         return data
     
-
+    # Decoding. Given an SSP S(x) return x. There are several method to do this. 
+    # method='from-set' (recommnded): reliable, but slow. Needs num_sample_pts to be quite large
+    #       ='direct-optim': can be faster than 'from-set' but not as reliable if num_init_pts is too low
+    # This will be updated in the future with some new faster methods 
     def decode(self,ssp,method='from-set', num_sample_pts=10000,from_set_method='grid',num_init_pts =10):
         if method=='least-squares':
             # problems duw to complex log
@@ -106,11 +134,14 @@ class SSPSpace:
         else:
             raise NotImplementedError()
         
+    # Given a vector S, decoding x from S, then encode S(x) and return
+    # This is useful when S is not a perfect SSP but a noisey one that needs to be 'cleaned-up'
     def clean_up(self,ssp,**kwargs):
         x = self.decode(ssp,**kwargs)
         return self.encode(x)
         
-        
+    # Sample points from the domain \mathcal{X}
+    # Two methods: 'grid' (return points gridded over n-dim space) or 'sobol' (quasi-random numbers that cover space well)
     def get_sample_points(self,num_points,method='grid'):
         if self.domain_bounds is None:
             bounds = np.vstack([-10*np.ones(self.domain_dim), 10*np.ones(self.domain_dim)]).T
@@ -140,20 +171,23 @@ class SSPSpace:
             raise NotImplementedError()
             
         
-    
+    # Get sample SSPs. Calls get_sample_points then encodes the result
     def get_sample_ssps(self,num_points,**kwargs): # make new if num_pts different than whats stored?
         sample_points = self.get_sample_points(num_points,**kwargs)
         sample_ssps = self.encode(sample_points)
         return sample_ssps, sample_points
     
+    # Normalize a vector. Note: all SSPs are unit length but this might be useful when dealing with noisey SSPs or sums of SSPs
     def normalize(self,ssp):
         return ssp/np.sqrt(np.sum(ssp**2))
     
+    # Make a vector unitary. Note: all SSPs are unitary but this might be useful when dealing with noisey SSPs or sums of SSPs
     def unitary(self,ssp):
         fssp = np.fft.fft(ssp)
         fssp = fssp/np.sqrt(fssp.real**2 + fssp.imag**2)
         return np.fft.ifft(fssp).real 
 
+    # Rather than decode a single point, decode a whole sequence of SSPs
     def decode_path(self, ssp_path, N_ma=None, n_samples = 10000):
         sample_ssps, sample_points = self.get_sample_ssps(n_samples)
         path = np.zeros((ssp_path.shape[0], self.domain_dim))
@@ -165,6 +199,12 @@ class SSPSpace:
         
         return path, max_sims
     
+    # Make a similarity plot. Given an input vector 'ssp', compute its simiarity with SSPs representing 'n_grid' points tiled over the \mathcal{X} space
+    # and plot these simiarities over space. This is a method for visualizing these high-dim SSP vectors 
+    # For SSPs representing 1D data, this will be a line plot
+    # For SSPs representing 2D data, this can be plotted as a heatmap, contour, or contourf plot (use keywarg 'plot_type')
+    #           (Note: such information could also be visulized by a surface plot, but that is not included here)
+    # This does not work for n>2
     def similarity_plot(self,ssp,n_grid=100,plot_type='heatmap',cmap="YlGnBu",ax=None,**kwargs):
         if ax is None:
             fig = plt.figure()
@@ -189,16 +229,36 @@ class SSPSpace:
             raise NotImplementedError()
         return ax
         
-        
+###################################################
+#  Subclasses for using particular phase_matrix A #
+####################################################   
             
-            
+          
+#################################################
+#  RandomSSPSpace uses a random phase_matrix A #
+#################################################
+# No extra parameters are requried for this subclass. rng is an optional kwarg for specfiying a random generator for reproducibility
+# This will construct a random A is such a way that exp(iA) will have conjugate symmertry so resulting SSPs will be real-valued
 class RandomSSPSpace(SSPSpace):
     def __init__(self, domain_dim: int, ssp_dim: int,  domain_bounds=None, length_scale=1, rng=np.random.default_rng()):
         partial_phases = rng.random.rand(ssp_dim//2,domain_dim)*2*np.pi - np.pi
         axis_matrix = _constructaxisfromphases(partial_phases)
         super().__init__(domain_dim,ssp_dim,axis_matrix=axis_matrix,
                        domain_bounds=domain_bounds,length_scale=length_scale)
-        
+
+##############################################################
+#  HexagonalSSPSpace uses a special structured phase_matrix A #
+##############################################################
+# This constructs A as follows
+#       Compute the vectors that make up an n-simplex inscribed in a unit hypersphere
+#       Create 'n_scales' scalar values from 'scale_min' to 'scale_max'
+#       Create 'n_rotates' rotation matrices 
+#       Create all permutations of scaling and rotating the vectors that make up the simplex
+#       Concatenate all resulting vectors together to create a ((n+1)*n_rotates*n_scales) x n matrix
+#       Make that matrix have conjugate symmertry, resulting in a (2*(n+1)*n_rotates*n_scales + 1) x n matrix
+#       That matrix is A
+# SSPs defined by such a A have esrieable properties, including smoother similairty plots. They can be represented by populations of grid cells too
+# See "Accurate representation for spatial cognition using grid cells" for more details
 class HexagonalSSPSpace(SSPSpace):
     def __init__(self,  domain_dim:int,ssp_dim: int=151, n_rotates:int=5, n_scales:int=5, 
                  scale_min=2*np.pi/np.sqrt(6) - 0.5, scale_max=2*np.pi/np.sqrt(6) + 0.5,
@@ -236,6 +296,7 @@ class HexagonalSSPSpace(SSPSpace):
         super().__init__(domain_dim,ssp_dim,axis_matrix=axis_matrix,
                        domain_bounds=domain_bounds,length_scale=length_scale)
   
+    # This returns vectors which can be used as encoders in nengo networks to obtain grid cells
     def sample_grid_encoders(self, n):
         sample_pts = self.get_sample_points(n)
         N = self.num_grids
@@ -251,7 +312,9 @@ class HexagonalSSPSpace(SSPSpace):
             encoders[:,i] = N * proj_mat @ sub_space.encode(sample_pts[:,i])
         return encoders
 
-    
+####################
+# Helper functions #
+####################
     
 def _constructaxisfromphases(K):
     d = K.shape[0]
